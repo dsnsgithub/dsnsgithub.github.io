ca125d8cc285b74e1677d4826db32d1853b616a3
DSNS <48170013+dsnsgithub@users.noreply.github.com>
2020-11-20 16:06:47 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function darkMode() {
+
+    var element = document.getElementById("html");
+    var button = document.getElementById("darkbutton")
+    var elementclass = element.className;
+    
+    if (elementclass == "darkmode") {
+		element.classList.remove("darkmode");
+		button.innerHTML = '<i class="fas fa-moon"></i>';
+	} else {
+		element.classList.add("darkmode");
+		button.innerHTML = '<i class="fas fa-sun"></i>';
+	}
+	
+}
