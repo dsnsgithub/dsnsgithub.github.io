6361cd12eae7ef42d57c7a0e3a868084322cc762
DSNS <48170013+dsnsgithub@users.noreply.github.com>
2020-11-23 16:13:46 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ function darkMode() {
     var element = document.getElementById("html");
     var button = document.getElementById("darkbutton")
     var elementclass = element.className;
-    
+
+    //var twitterEmbed = document.getElementsByName("blockquote");
+
+
     if (elementclass == "darkmode") {
 		element.classList.remove("darkmode");
-		button.innerHTML = '<i class="fas fa-moon"></i>';
+        button.innerHTML = '<i class="fas fa-moon"></i>';
+        //twitterEmbed.setAttribute("data-theme", "light");
 	} else {
 		element.classList.add("darkmode");
-		button.innerHTML = '<i class="fas fa-sun"></i>';
+        button.innerHTML = '<i class="fas fa-sun"></i>';
+       // twitterEmbed.setAttribute("data-theme", "dark")
 	}
 	
 }
