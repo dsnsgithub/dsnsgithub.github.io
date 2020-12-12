0e760742eb4474abd1b0f86de374f6a9b5f8f643
DSNS <48170013+dsnsgithub@users.noreply.github.com>
2020-12-11 20:42:38 -0800

    Add files via upload

---
+++ pi/index.js
@@ -0,0 +1,18 @@
+file = "https://dsnsgithub.github.io/pi/pi.txt";
+var rawFile = new XMLHttpRequest();
+rawFile.open("GET", file, false);
+rawFile.onreadystatechange = function () {
+	if (rawFile.readyState === 4) {
+		if (rawFile.status === 200 || rawFile.status == 0) {
+            var pitxt = rawFile.responseText;
+            for (i = 0; i < pitxt.length; i++) {
+				setTimeout(() => {
+					pidisplay = document.getElementById("pidisplay");
+					pidisplay = pitxt[i];
+				}, 1000);
+			}
+		}
+	}
+};
+rawFile.send(null);
+
