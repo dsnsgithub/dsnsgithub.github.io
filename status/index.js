9eeb93c895349aa1228fe2f12edcdae0186d79c2
DSNS <48170013+dsnsgithub@users.noreply.github.com>
2021-02-26 15:53:10 -0800

    Add files via upload

--- status/index.js
+++ status/index.js
@@ -2,19 +2,12 @@ let DSNS = document.getElementById("DSNS");
 let AmKale = document.getElementById("AmKale");
 let jiebi = document.getElementById("jiebi");
 
-
-
 let DSNSgame = document.getElementById("DSNSgame");
 let jiebigame = document.getElementById("jiebigame");
 let AmKalegame = document.getElementById("AmKalegame");
 
-
 async function getStatus(playerName) {
-	let response = await (
-		await fetch(
-			`https://api.slothpixel.me/api/players/${playerName}/status`
-		)
-	).json();
+	let response = await (await fetch(`https://api.slothpixel.me/api/players/${playerName}/status`)).json();
 
 	if (response["online"]) {
 		return `${playerName} is online. They are playing ${response["game"]["mode"]} ${response["game"]["type"]}.`;
@@ -24,21 +17,16 @@ async function getStatus(playerName) {
 }
 
 async function getRecentGame(playerName) {
-	let response = await (
-		await fetch(
-			`https://api.slothpixel.me/api/players/${playerName}/recentGames`
-		)
-	).json();
-	
-	time = new Date(response[0]["date"]).toString("en-US", {
+	let response = await (await fetch(`https://api.slothpixel.me/api/players/${playerName}/recentGames`)).json();
+
+	time = new Date(response[0]["date"]).toLocaleDateString("en-US", {
 		hour: "numeric",
 		minute: "numeric",
-		hour12: true,
+		hour12: true
 	});
 	return `${playerName} played ${response[0]["mode"]} ${response[0]["gameType"]} at ${time}.`;
-    
 
-    return string
+	return string;
 }
 
 async function run() {
@@ -46,10 +34,9 @@ async function run() {
 	AmKale.innerHTML = await getStatus("AmKale");
 	jiebi.innerHTML = await getStatus("jiebi");
 
-    DSNSgame.innerHTML = await getRecentGame("DSNS");
-    jiebigame.innerHTML = await getRecentGame("jiebi");
-    AmKalegame.innerHTML = await getRecentGame("AmKale");
-    
+	DSNSgame.innerHTML = await getRecentGame("DSNS");
+	jiebigame.innerHTML = await getRecentGame("jiebi");
+	AmKalegame.innerHTML = await getRecentGame("AmKale");
 }
 
 run();
